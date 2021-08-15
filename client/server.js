197c4b3a5d48e632e2b758d9270e287f59179ce7
leee1846 <leee1846@gmail.com>
2021-08-15 18:03:45 +0900

    feat: server.js 적용

--- client/server.js
+++ client/server.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const port = 8080;
 
-app.use(express.static(__dirname + "./"));
-app.use(express.static(__dirname + "./css"));
+app.use(express.static(__dirname + "/src"));
 
-// 라우팅 정의
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-// 서버 실행
-app.listen(PORT, () => {
-  console.log(`Listen : ${PORT}`);
+app.listen(port, () => {
+  console.log(`서버가 ${port}에서 실행중...`);
 });
